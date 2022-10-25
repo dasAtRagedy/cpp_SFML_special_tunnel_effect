5fbdac624bc007c152464982bd3b9fd7346e2735
Dunix <taiTragedija@gmail.com>
2022-10-25 17:40:56 +0300

    Cube can be moved with mouse

--- main.cpp
+++ main.cpp
@@ -2,14 +2,15 @@
 #include <SFML/graphics.hpp>
 
 int main() {
-    setbuf(stdout, NULL);
+    //setbuf(stdout, nullptr);
 
     sf::RenderWindow window(sf::VideoMode(512, 512), "SFML Toot");
     sf::RectangleShape player(sf::Vector2f(100, 100));
     player.setFillColor(sf::Color::Magenta);
+    player.setOrigin(50.0f, 50.0f);
 
     while(window.isOpen()){
-        sf::Event e;
+        sf::Event e{};
         while(window.pollEvent(e)){
             switch(e.type){
                 case sf::Event::Closed:
@@ -23,16 +24,9 @@ int main() {
                     break;
             }
         }
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)){
-            player.move(0.0f, -0.1f);
-        }if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)){
-            player.move(-0.1f, 0.0f);
-        }if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S)){
-            player.move(0.0f, 0.1f);
-        }if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D)){
-            player.move(0.1f, 0.0f);
-        }
 
+        sf::Vector2i mousePos = sf::Mouse::getPosition(window);
+        player.setPosition(static_cast<float>(mousePos.x), static_cast<float>(mousePos.y));
         window.clear();
         window.draw(player);
         window.display();
