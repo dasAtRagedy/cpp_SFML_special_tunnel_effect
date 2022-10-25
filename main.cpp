4f0c09ec78394e26acce15e8ea518462714b369a
Dunix <taiTragedija@gmail.com>
2022-10-25 22:27:28 +0300

    Similar effect to the example accomplished, no delay; `Escape` now quits program TODO: Add delay after each cube

--- main.cpp
+++ main.cpp
@@ -1,13 +1,24 @@
 #include <iostream>
 #include <SFML/graphics.hpp>
+#include <math.h>
+
+#define screen_size 512.0f
+#define scarcity 0.9f
+#define n 50
 
 int main() {
-    //setbuf(stdout, nullptr);
+    // setbuf(stdout, nullptr);
 
-    sf::RenderWindow window(sf::VideoMode(512, 512), "SFML Toot");
-    sf::RectangleShape player(sf::Vector2f(100, 100));
-    player.setFillColor(sf::Color::Magenta);
-    player.setOrigin(50.0f, 50.0f);
+    sf::RenderWindow window(sf::VideoMode(screen_size, screen_size), "", sf::Style::None);
+    std::vector<sf::RectangleShape> cubes;
+    for(int i = 0; i < n; i++){
+        sf::RectangleShape cube_prefab(sf::Vector2f(screen_size * pow(scarcity, i+1), screen_size * pow(scarcity, i+1)));
+        cube_prefab.setOrigin(screen_size / 2.0f, screen_size / 2.0f);
+        cube_prefab.setFillColor(sf::Color::Black);
+        cube_prefab.setOutlineColor(sf::Color::Green);
+        cube_prefab.setOutlineThickness(1.0f);
+        cubes.push_back(cube_prefab);
+    }
 
     while(window.isOpen()){
         sf::Event e{};
@@ -16,19 +27,20 @@ int main() {
                 case sf::Event::Closed:
                     window.close();
                     break;
-                case sf::Event::Resized:
-                    printf("%i %i\n", e.size.width, e.size.height);
-                    break;
-                case sf::Event::TextEntered:
-                    printf("%c", e.text.unicode);
-                    break;
             }
         }
 
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Escape))
+            window.close();
+
         sf::Vector2i mousePos = sf::Mouse::getPosition(window);
-        player.setPosition(static_cast<float>(mousePos.x), static_cast<float>(mousePos.y));
+        for(int i = 0; i < n; i++)
+            cubes[i].setPosition(
+                    (static_cast<float>(mousePos.x) * (1 - pow(scarcity, i+1)) + screen_size / 2),
+                    (static_cast<float>(mousePos.y) * (1 - pow(scarcity, i+1)) + screen_size / 2));
         window.clear();
-        window.draw(player);
+        for(int i = 0; i < n; i++)
+            window.draw(cubes[i]);
         window.display();
     }
 
