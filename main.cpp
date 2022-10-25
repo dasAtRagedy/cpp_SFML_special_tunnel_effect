80a151129b9ff480a31191d1d59e9f0c3a2b864e
Dunix <taiTragedija@gmail.com>
2022-10-25 17:19:17 +0300

    Now we have a cube that can be moved around the canvas with WASD.

--- main.cpp
+++ main.cpp
@@ -2,21 +2,39 @@
 #include <SFML/graphics.hpp>
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    setbuf(stdout, NULL);
 
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
+    sf::RenderWindow window(sf::VideoMode(512, 512), "SFML Toot");
+    sf::RectangleShape player(sf::Vector2f(100, 100));
+    player.setFillColor(sf::Color::Magenta);
+
+    while(window.isOpen()){
+        sf::Event e;
+        while(window.pollEvent(e)){
+            switch(e.type){
+                case sf::Event::Closed:
+                    window.close();
+                    break;
+                case sf::Event::Resized:
+                    printf("%i %i\n", e.size.width, e.size.height);
+                    break;
+                case sf::Event::TextEntered:
+                    printf("%c", e.text.unicode);
+                    break;
+            }
+        }
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)){
+            player.move(0.0f, -0.1f);
+        }if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)){
+            player.move(-0.1f, 0.0f);
+        }if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S)){
+            player.move(0.0f, 0.1f);
+        }if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D)){
+            player.move(0.1f, 0.0f);
         }
 
         window.clear();
-        window.draw(shape);
+        window.draw(player);
         window.display();
     }
 
